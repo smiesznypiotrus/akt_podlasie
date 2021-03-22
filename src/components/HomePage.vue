ef8ebe92d876a4b6de9958869625255e6c05dd1a
smiesznypiotrus <piotr.pyciak@gmail.com>
2021-03-22 14:06:54 +0100

    background image not responsive

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,16 @@
 <template>
+    <div class="container-fluid m-0 p-0">
+      <div id="header" class="full-height p-4">
 
+      </div>
+      <div id="content" class="bg-info p-4">
+
+      </div>
+      <div id="footer" class="bg-warning p-4">
+
+      </div>
+      
+    </div>
 </template>
 
 <script>
@@ -12,5 +23,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  #header {
+    background: url('../assets/header.png') no-repeat center center fixed;    
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
 </style>
